a27b74b1e67a0a3eb4ecb53a1706218f7992ed2a
alfalinux <alfahmi.asman@gmail.com>
2022-11-08 03:46:43 +0700

    Header image to public

--- components-user/Jumbotron.js
+++ components-user/Jumbotron.js
@@ -8,7 +8,7 @@ const Jumbotron = () => {
       <div>
         <img
           className={styles["jumbo-img"]}
-          src="https://images.unsplash.com/photo-1580674285054-bed31e145f59?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+          src="images/jumbotron.jpg"
           alt="kepercayaan konsumen"
         ></img>
       </div>
